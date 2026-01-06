07700f21eb108e5d1c80053459ad70f16e97ff05
Long Truong <ngoclong31120@gmail.com>
2026-01-06 18:04:16 +0700

    init repo

---
+++ template.py
@@ -0,0 +1,19 @@
+"""
+Author: Long Truong
+Date:
+Problem title:
+Url:
+"""
+
+class Solution:    
+  def method_name(self, param1: type, param2: type) -> return_type:
+    """
+    Brief description of the method.
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
